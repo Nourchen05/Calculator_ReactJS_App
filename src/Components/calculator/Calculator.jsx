c41cc543b2fc1724d5d30734d77a509a8cbe9c46
Nourchen05 <noamennourchen5@gmail.com>
2021-11-02 23:32:59 +0100

    first commit

---
+++ src/Components/calculator/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Calculator.css";
+
+const Calculator = () => {
+  return (
+    <div>
+      <p>Hello Nourchen</p>
+    </div>
+  );
+};
+
+export default Calculator;
