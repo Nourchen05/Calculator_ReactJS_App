3341a98821600af5709ef72f241469112a4334d2
Nourchen05 <noamennourchen5@gmail.com>
2021-11-03 00:26:12 +0100

    second commit

--- src/Components/calculator/Calculator.jsx
+++ src/Components/calculator/Calculator.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Calculator.css";
+import { btns, BTN_ACTIONS } from "./btnConfig";
 
 const Calculator = () => {
   return (
-    <div>
-      <p>Hello Nourchen</p>
+    <div className="calculator">
+      <div className="calculator_result">
+        <div className="calculator_result_exp"></div>
+        <div className="calculator_result_exp"></div>
+      </div>
+      <div className="calculator_btns">
+        {btns.map((item, index) => (
+          <button key={index} className={item.class}>
+            {item.display}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/Components/calculator/btnConfig.js
@@ -0,0 +1,109 @@
+export const BTN_ACTIONS = {
+  ADD: "ADD",
+  THEME: "THEME",
+  CALC: "CALC",
+  DELETE: "DELETE",
+};
+
+export const btns = [
+  {
+    display: "C",
+    action: "BTN_ACTIONS.DELETE",
+    CLASS: "btn_op",
+  },
+  {
+    display: "(",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: ")",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "/",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "7",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "8",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "9",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "x",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__op",
+  },
+  {
+    display: "4",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "5",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "6",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "-",
+    action: BTN_ACTIONS.ADD,
+    class: "btn__op",
+  },
+  {
+    display: "1",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "2",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "3",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "+",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "btn_op",
+  },
+  {
+    display: "☀️",
+    action: "BTN_ACTIONS.THEME",
+    CLASS: "",
+  },
+  {
+    display: "0",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "",
+  },
+  {
+    display: ".",
+    action: "BTN_ACTIONS.ADD",
+    CLASS: "",
+  },
+  {
+    display: "=",
+    action: "BTN_ACTIONS.CALC",
+    CLASS: "btn_op",
+  },
+];
