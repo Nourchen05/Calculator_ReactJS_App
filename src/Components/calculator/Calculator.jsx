07df81ddee9b2f0aebc65e6a31088adf100ad0f0
Nourchen05 <noamennourchen5@gmail.com>
2021-11-03 01:16:35 +0100

    fourth commit

--- src/Components/calculator/Calculator.jsx
+++ src/Components/calculator/Calculator.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./Calculator.css";
 import { btns, BTN_ACTIONS } from "./btnConfig";
 
 const Calculator = () => {
+  const btnsRef = useRef(null);
+  const expRef = useRef(null);
+  useEffect(() => {
+    const btns = Array.from(btnsRef.current.querySelectorAll("button"));
+    btns.forEach((e) => (e.style.height = e.offsetWidth + "px"));
+  }, []);
+  const btnClick = (item) => {
+    console.log(item);
+    const exDiv = expRef.current;
+    if (item.action === BTN_ACTIONS.THEME)
+      document.body.classList.toggle("dark");
+  };
   return (
     <div className="calculator">
       <div className="calculator_result">
-        <div className="calculator_result_exp"></div>
+        <div ref={expRef} className="calculator_result_exp">
+          9999
+        </div>
         <div className="calculator_result_exp"></div>
       </div>
-      <div className="calculator_btns">
+      <div ref={btnsRef} className="calculator_btns">
         {btns.map((item, index) => (
-          <button key={index} className={item.class}>
+          <button
+            key={index}
+            className={item.class}
+            onClick={() => btnClick(item)}
+          >
             {item.display}
           </button>
         ))}
